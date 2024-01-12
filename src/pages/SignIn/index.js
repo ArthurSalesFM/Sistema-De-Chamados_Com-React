ada197c6c23bc2ebed277c5d2adf36302e71a902
ArthurSalesFM <sales.arthur.fm@gmail.com>
2024-01-12 15:26:29 -0300

    Enviando a página de login feita, tanto a parte do HTML quanto a do CSS

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,51 @@
-
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignIn(){
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
     return(
-        <div>
-            <h1>Página Login.</h1>
+        <div className='container-center'>
+
+            <div className='login'>
+
+                <div className='login-area'>
+
+                    <img 
+                    src={logo} 
+                    alt='Logo do sistema de chamados'/>
+
+                </div>
+
+                <form>
+
+                    <h1>Entrar</h1>
+
+                    <input 
+                    type='email' 
+                    placeholder='Digite seu Email...' 
+                    value={email} 
+                    onChange={(event) => setEmail(event.target.value)}/>
+                    
+                    <input 
+                    type='password' 
+                    placeholder='Digite sua Senha...' 
+                    value={senha} 
+                    onChange={(event) => setSenha(event.target.value)}/>
+                
+                    <button type='submit'>Acessar</button>
+
+                </form>
+
+                <Link to='/register'>Cadastre-se</Link>
+
+            </div>
+
         </div>
+
     );
 }
